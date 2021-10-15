68eb5894000a83d0e13a9dbeb2b7ef5a7214a05e
Srinivas Joshi <69837749+SrinivasJoshi@users.noreply.github.com>
2021-10-15 19:22:43 +0530

    social media icons added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import * as styles from '../styles/footer.module.css' 
+import { StaticImage } from 'gatsby-plugin-image';
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-           <p>Social media Icons</p>
+           <div className={styles.socialGrp}>
+               <a href="https://github.com/SrinivasJoshi"><StaticImage src="../images/github-icon.svg" className={styles.socialIcon} /> </a>
+               <a href="https://twitter.com/JustSrinivas12"><StaticImage src="../images/twitter-icon.svg" className={styles.socialIcon} /> </a>
+               <a href="https://www.linkedin.com/in/srinivas-joshi-a3abbb1b5"><StaticImage src="../images/linkedin-icon.svg" className={styles.socialIcon} /> </a>
+           </div>
+
            <p>Copyright : 2021  Srinivas Joshi</p> 
            <p><a href="#home">Go to Top </a></p>
         </footer>
